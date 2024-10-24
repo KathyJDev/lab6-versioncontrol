0d5560c581f6def106ff7b576dcf4597bffbff35
BaterWottleDoggo <baterwottle@protonmail.com>
2024-10-23 17:34:11 -0700

    decode function

--- version_control.py
+++ version_control.py
@@ -2,8 +2,13 @@
 def encode(password):
     result = ""
     for i in range(len(password)):
-        result += str(int(password[i]) + 3)
+        result += str((int(password[i]) + 3) % 10)
     return result
+
+def decode(password):
+    for i in range(9):
+        password = encode(password)
+    return password
 
 if __name__ == "__main__":
     password = ""
@@ -18,12 +23,12 @@ if __name__ == "__main__":
         if option == 1:
             password = input("Please enter a password to encode: ")
             try:
-                encoded_password = encode(password)
+                password = encode(password)
             except:
                 print(f"Your password failed to encode.")
             else:
                 print("Your password has been encoded and stored!")
         elif option == 2:
-            print()
+            print("The encoded password is " +password + ", and the original password is " + decode(password) + ".")
         elif option == 3:
             break
